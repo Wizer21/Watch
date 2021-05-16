203cde97fcc572f81ee233526fbac61748778c52
Wizer <simon.tessier8@gmail.com>
2021-05-16 17:11:45 +0200

    Needle Scene

--- src/components/Scene3D.vue
+++ src/components/Scene3D.vue
@@ -16,7 +16,8 @@ export default {
 
     // render
     const renderer = new THREE.WebGLRenderer({
-      canvas: canvas
+      canvas: canvas,
+      antialias: true
     })
     renderer.setSize(sizes.width, sizes.height * 2)
     renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
@@ -33,17 +34,18 @@ export default {
     // Set Default Sizes
     setSizes()
 
-    // light 
-    const ambientLight = new THREE.AmbientLight( 0xffffff, 2)
-    const pointLight = new THREE.PointLight( 0xffffff, 3, 20)
-    pointLight.position.set(-6, 0, 2)
-
     // ----------
     // PART 1 
     // ----------
     const scene1 = new THREE.Scene()
-    scene1.add(ambientLight)
-    scene1.add(pointLight)
+
+    // light 
+    const ambientLight = new THREE.AmbientLight( 0xffffff, 2)
+
+    const pointLight = new THREE.PointLight( 0xffffff, 3, 20)
+    pointLight.position.set(-6, 0, 2)
+
+    scene1.add(ambientLight, pointLight)
 
     // mesh
     gltfLoader.load('./watch.glb', gltf => {
@@ -63,22 +65,35 @@ export default {
     // ----------
     const scene2 = new THREE.Scene()
 
+    // light 
+    const ambientLight2 = new THREE.AmbientLight( 0xffffff, 15)
+    
+    const pointLight2 = new THREE.PointLight( 0xffffff, 50, 20)
+    pointLight2.position.set(-1, 0, 2)
+
+    scene2.add(ambientLight2, pointLight2)
+
     // mesh
-    const cubeRed =  new THREE.Mesh(
-      new THREE.BoxGeometry(1, 1, 1),
-      new THREE.MeshBasicMaterial({ color: 0xff0000})
-    )
-    scene2.add(cubeRed)
+    gltfLoader.load('./needle.glb', gltf => {
+      const needle = gltf.scene
+
+      needle.scale.set(6, 6, 6)
+      
+      needle.rotation.x = -Math.PI * 0.5
+      needle.rotation.z = Math.PI
+
+      scene2.add(needle)
+    })
 
     const tick = () => {
       
       // Scissor Scenes
-      renderer.setScissor(0, 0, sizes.width, sizes.height)
-      renderer.setViewport(0, 0, sizes.width, sizes.height)
-      renderer.render(scene1, camera)
-
       renderer.setScissor(0, sizes.height, sizes.width, sizes.height)
       renderer.setViewport(0, sizes.height, sizes.width, sizes.height)
+      renderer.render(scene1, camera)
+
+      renderer.setScissor(0, 0, sizes.width, sizes.height)
+      renderer.setViewport(0, 0, sizes.width, sizes.height)
       renderer.render(scene2, camera)
 
       window.requestAnimationFrame(tick)
